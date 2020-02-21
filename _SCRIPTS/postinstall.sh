e5a7fd7738325fec4f485f2ba7bad525179af8b3
Nen <nen.work@protonmail.com>
2020-02-21 19:38:46 +0300

    Working postinstall

--- _SCRIPTS/postinstall.sh
+++ _SCRIPTS/postinstall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo 'IT WORKS' /home/mpeiadmin/ITWORKS
+# You are now in directory /root
+# place here whatever you want to run for changing new OS
+
+echo 'IT WORKS' /home/*/ITWORKS
+
 exit 0
